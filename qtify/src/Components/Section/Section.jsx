99b27a6f5fd5922b6875d8346770023bd9bde909
Shivangi Sharma <shanussh@gmail.com>
2023-12-30 00:27:24 +0530

    Songs

--- qtify/src/Components/Section/Section.jsx
+++ qtify/src/Components/Section/Section.jsx
@@ -1,36 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Section.module.css";
 import { CircularProgress } from "@mui/material";
 import Card from "../Card/Card";
 import { useState } from "react";
 import Carousel from "../Carousel/Carousel";
+import Filters from "../Filters/Filters";
 
-export default function Section({ title, data, type }) {
+export default function Section({ title, data, filterSource, type }) {
   const [carouselToggle, setCarouselToggle] = useState(true);
+  const [filters, setFilters] = useState({ key: "all", label: "All" });
+  const [selectedFilterIndex, setSelectedFilterIndex] = useState(0);
   const handleToggle = () => {
     setCarouselToggle((prevState) => !prevState);
   };
+
+  useEffect(() => {
+    if (filterSource) {
+      filterSource().then((response) => {
+        const { data } = response;
+        setFilters([...filters, ...data]);
+      });
+    }
+  }, []);
+
+  const showFilters = filters.length > 1;
+  const cardsToRender = data.filter((card) =>
+    showFilters && selectedFilterIndex !== 0
+      ? card.genre.key === filters[selectedFilterIndex].key
+      : card
+  );
   return (
     <div>
       <div className={styles.header}>
         <h3>{title}</h3>
-        <h4 className={styles.toggleText} onClick={handleToggle}>
-          {!carouselToggle ? "Collapse All" : "Show All"}
-        </h4>
+        {!showFilters && (
+          <h4 className={styles.toggleText} onClick={handleToggle}>
+            {!carouselToggle ? "Collapse All" : "Show All"}
+          </h4>
+        )}
       </div>
-      {data.length === 0 ? (
+      {showFilters && (
+        <div className={styles.filterWrapper}>
+          <Filters
+            filters={filters}
+            selectedFilterIndex={selectedFilterIndex}
+            setSelectedFilterIndex={setSelectedFilterIndex}
+          />
+        </div>
+      )}
+      {cardsToRender.length === 0 ? (
         <CircularProgress />
       ) : (
         <div className={styles.cardWrapper}>
           {!carouselToggle ? (
             <div className={styles.wrapper}>
-              {data.map((ele) => (
+              {cardsToRender.map((ele) => (
                 <Card data={ele} type={type} />
               ))}
             </div>
           ) : (
             <Carousel
-              data={data}
+              data={cardsToRender}
               renderComponent={(data) => <Card data={data} type={type} />}
             />
           )}
--- qtify/src/api/api.js
+++ qtify/src/api/api.js
@@ -22,7 +22,7 @@ export const fetchNewAlbums = async () => {
 
 export const fetchSongs = async () => {
   try {
-    const response = await axios.get(`${BACKEND_ENDPOINT}/albums/songs`);
+    const response = await axios.get(`${BACKEND_ENDPOINT}/songs`);
     return response.data;
   } catch (e) {
     console.error(e);
@@ -31,7 +31,7 @@ export const fetchSongs = async () => {
 
 export const fetchFilters = async () => {
   try {
-    const response = await axios.get(`${BACKEND_ENDPOINT}/albums/genres`);
+    const response = await axios.get(`${BACKEND_ENDPOINT}/genres`);
     return response.data;
   } catch (e) {
     console.error(e);
